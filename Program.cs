db8a8a0fe7054687acc1c395b5efaf811dbade6b
RavulaSonali <ravula.sonali@gmail.com>
2023-11-21 16:57:45 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ConAppAss17Part1;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount myAccount = new BankAccount("Ravula Sonali");
+
+        Console.WriteLine($"Account Number: {myAccount.AccountNumber}");
+        Console.WriteLine($"Account Holder: {myAccount.AccountHolderName}");
+        Console.WriteLine($"Initial Balance: ${myAccount.Balance}");
+
+        myAccount.Deposit(1000);
+        myAccount.Withdraw(500);
+
+        Console.WriteLine($"Final Balance: ${myAccount.Balance}");
+        Console.ReadKey();
+    }
+}
